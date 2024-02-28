b01c1cbdd7e489abdf610b688d6c1031b5d17f57
mishra06 <mishra.vibhakar7262@gmail.com>
2024-02-28 12:32:17 +0530

    1st react project

---
+++ index.js
@@ -0,0 +1,7 @@
+const H2 = React.createElement("h2",{},"hello mishra")
+const P = React.createElement('p',{},"i am vibhakarmishra")
+const Img = React.createElement('img',{src:'https://media.istockphoto.com/id/174812956/photo/dozen-roses.jpg?s=612x612&w=0&k=20&c=G30YG1YC3uAyNVVEPHM6YWVMuZJZ5vcjZ5iVrpi5jgk=',alt:'rose'})
+
+const Div = React.createElement('div',{},[H2,P,Img])
+
+ReactDOM.render(Div,document.getElementById('root'));
