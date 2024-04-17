30186aaefd7ddc5079bba83e25f27028fce63f28
mishra06 <mishra.vibhakar7262@gmail.com>
2024-04-17 19:10:01 +0530

    done

---
+++ react.js
@@ -0,0 +1,19 @@
+const A = React.createElement("h1", {}, "Topics Coverd")
+
+  const B = React.createElement("a", {href:'#'}, "hhfufikif")
+
+  const C = React.createElement("p", {}, "If you are a complete beginner, web development can be challenging - we will hold your hand and provide enough detail for you to feel comfortable and learn the")
+
+  const X = React.createElement("a", {href:'#'}, "hhfufikif")
+
+  const Y = React.createElement("p", {}, "If you are a complete beginner, web development can be challenging - we will hold your hand and provide enough detail for you to feel comfortable and learn the")
+
+  const M = React.createElement("a", {href:'#'}, "hhfufikif")
+
+  const N = React.createElement("p", {}, "If you are a complete beginner, web development can be challenging - we will hold your hand and provide enough detail for you to feel comfortable and learn the")
+
+  const Div = React.createElement("div", {}, [A, B, C, X, Y, M, N])
+
+
+
+  ReactDOM.render(Div, document.getElementById("root1"));
